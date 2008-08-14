a76ba5c51d34a6f520ea6a2173951a6955271d90
Henning Jensen <henning.jensen@gmail.com>
2008-08-14 07:54:43 +0000

    broke the trunk, branch, tag structure. removing everything

--- trunk/plugin/src/main/java/no/bekk/boss/bpep/test/Person.java
+++
@@ -1,6 +0,0 @@
-package no.bekk.boss.bpep.test;
-
-public class Person {
-    private String firstname;
-    private String lastname;
-}
--- trunk/plugin/src/test/java/no/bekk/boss/bpep/example/PersonCreator.java
+++
@@ -1,8 +0,0 @@
-package no.bekk.boss.bpep.example;
-
-public class PersonCreator {
-	public static void main(String[] args) {
-		Person person = new PersonBuilder().lastname("jensen").build();
-		person.toString();
-	}
-}
