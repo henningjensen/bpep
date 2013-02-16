2e684c41d73120204b4c85e4f0be7d8372de1b20
Ville Saalo <ZeroOne@iki.fi>
2013-02-16 15:36:17 +0200

    Builder methods shouldn't use field prefix or suffix
    Fixed issue 10, "Builder methods shouldn't use field prefix or suffix", from the original Google Code issue tracker of this project (http://code.google.com/p/bpep/issues/detail?id=10).

--- plugin/src/main/java/no/bekk/boss/bpep/generator/BuilderGenerator.java
+++ plugin/src/main/java/no/bekk/boss/bpep/generator/BuilderGenerator.java
@@ -5,13 +5,17 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IBuffer;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.NamingConventions;
 import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
@@ -23,6 +27,12 @@ import org.eclipse.text.edits.TextEdit;
 public class BuilderGenerator implements Generator {
 
 	private static final String BUILDER_METHOD_PARAMETER_SUFFIX = "Param";
+
+	private final IJavaProject javaProject;
+
+	public BuilderGenerator() {
+		javaProject = JavaCore.create(ResourcesPlugin.getWorkspace().getRoot().getProject());
+	}
 
 	public void generate(ICompilationUnit cu, boolean createBuilderConstructor, boolean createCopyConstructor, boolean formatSource, List<IField> fields) {
 
@@ -139,12 +149,18 @@ public class BuilderGenerator implements Generator {
 
 	private void createBuilderMethods(PrintWriter pw, List<IField> fields) throws JavaModelException {
 		for (IField field : fields) {
-			String name = getName(field);
-			String type = getType(field);
-			pw.println("public Builder " + name + "(" + type + " " + name + BUILDER_METHOD_PARAMETER_SUFFIX + ") {");
-			pw.println("this." + name + "=" + name + BUILDER_METHOD_PARAMETER_SUFFIX + ";");
+			String fieldName = getName(field);
+			String fieldType = getType(field);
+			String baseName = getFieldBaseName(fieldName);
+			String parameterName = baseName + BUILDER_METHOD_PARAMETER_SUFFIX;
+			pw.println("public Builder " + baseName + "(" + fieldType + " " + parameterName + ") {");
+			pw.println("this." + fieldName + "=" + parameterName + ";");
 			pw.println("return this;\n}");
 		}
+	}
+
+	private String getFieldBaseName(String fieldName) {
+		return NamingConventions.getBaseName(NamingConventions.VK_INSTANCE_FIELD, fieldName, javaProject);
 	}
 
 	private void createFieldDeclarations(PrintWriter pw, List<IField> fields) throws JavaModelException {
